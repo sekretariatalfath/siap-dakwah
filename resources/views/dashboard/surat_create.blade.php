828f3a5aacc7b6d0ea5c7c0a7306e899c96f391c
MNaufalMuzakki <sekretariat.alfath@gmail.com>
2026-05-11 16:37:06 +0700

    fix: map komunikasi and sosial to FKS, restore FIK and FIT

--- resources/views/dashboard/surat_create.blade.php
+++ resources/views/dashboard/surat_create.blade.php
@@ -97,10 +97,9 @@
                                         elseif(str_contains($u, 'ELEKTRO')) $shortCode = "FTE";
                                         elseif(str_contains($u, 'INDUSTRI')) $shortCode = "FRI";
                                         elseif(str_contains($u, 'EKONOMI')) $shortCode = "FEB";
-                                        elseif(str_contains($u, 'KOMUNIKASI')) $shortCode = "FKB";
+                                        elseif(str_contains($u, 'KOMUNIKASI') || str_contains($u, 'SOSIAL')) $shortCode = "FKS";
                                         elseif(str_contains($u, 'KREATIF')) $shortCode = "FIK";
                                         elseif(str_contains($u, 'TERAPAN')) $shortCode = "FIT";
-                                        elseif(str_contains($u, 'SOSIAL')) $shortCode = "FKB";
                                         elseif(str_contains($u, 'KEDOKTERAN')) $shortCode = "FKD";
                                         else {
                                             $words = explode(' ', $unit);
